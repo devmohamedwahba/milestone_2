226461298d2f0355cb3dd5cc57fbe48904a82d38
devmohamedwahba <devmohamedwahba@gmail.com>
2021-01-13 16:06:45 +0200

    finish json

--- utils/database.py
+++ utils/database.py
@@ -1,38 +1,35 @@
-books_file = 'books.txt'
+import json
+
+books_file = 'books.json'
 
 
 def create_book_table():
-    with open(books_file, 'w'):
-        pass
+    with open(books_file, 'w') as file:
+        json.dump([], file)
 
 
 def add_book(name, author):
-    with open(books_file, 'a') as file:
-        file.write(f'{name},{author},0\n')
+    books = get_all_books()
+    books.append({"name": name, "author": author, "read": False})
+    _save_all_books(books)
 
 
 def get_all_books():
     with open(books_file, 'r') as file:
-        lines = [line.strip().split(',') for line in file.readlines()]
-
-    return [
-        {'name': line[0], 'author': line[1], 'read': line[2]}
-        for line in lines
-    ]
+        return json.load(file)
 
 
 def mark_book_as_read(name):
     books = get_all_books()
     for book in books:
         if book['name'] == name:
-            book['read'] = '1'
+            book['read'] = True
     _save_all_books(books)
 
 
 def _save_all_books(books):
     with open(books_file, 'w') as file:
-        for book in books:
-            file.write(f"{book['name']},{book['author']},{book['read']}\n")
+        json.dump(books, file,indent=2)
 
 
 def delete_book(name):
