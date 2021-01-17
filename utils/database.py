2573b8e0ae23cec1723779a9afb890704c8af4d3
devmohamedwahba <devmohamedwahba@gmail.com>
2021-01-17 14:33:20 +0200

    finish adding sqlite setting

--- utils/database.py
+++ utils/database.py
@@ -1,38 +1,33 @@
-import json
-
-books_file = 'books.json'
+from .database_connection import DatabaseConnection
 
 
 def create_book_table():
-    with open(books_file, 'w') as file:
-        json.dump([], file)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('CREATE TABLE IF NOT EXISTS books(name text primary key, author text, read integer)')
 
 
 def add_book(name, author):
-    books = get_all_books()
-    books.append({"name": name, "author": author, "read": False})
-    _save_all_books(books)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('INSERT INTO books VALUES(?, ?, 0)', (name, author))
 
 
 def get_all_books():
-    with open(books_file, 'r') as file:
-        return json.load(file)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('SELECT * FROM books')
+        books = [{"name": row[0], "author": row[1], "read": row[2]} for row in cursor.fetchall()]
+    return books
 
 
 def mark_book_as_read(name):
-    books = get_all_books()
-    for book in books:
-        if book['name'] == name:
-            book['read'] = True
-    _save_all_books(books)
-
-
-def _save_all_books(books):
-    with open(books_file, 'w') as file:
-        json.dump(books, file,indent=2)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('UPDATE books SET read=1 WHERE name=?', (name,))
 
 
 def delete_book(name):
-    books = get_all_books()
-    books = [book for book in books if book['name'] != name]
-    _save_all_books(books)
+    with DatabaseConnection('data.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute('DELETE FROM books WHERE name=?', (name,))
